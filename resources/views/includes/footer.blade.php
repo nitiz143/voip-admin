91566d79f6ef095958089f393fd9a919313c6e02
Nitiz Sharma <nitiz143@users.noreply.github.com>
2022-05-02 19:18:27 +0530

    update

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 </strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+    </div>
+  </footer>
+
+   <!-- Control Sidebar -->
+   <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
