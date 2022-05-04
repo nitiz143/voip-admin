804bdfbcdb02847e4fcfd823ffb31d7f51cb7e36
Nitiz Sharma <nitiz143@users.noreply.github.com>
2022-05-04 17:48:34 +0530

    update

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -18,61 +18,65 @@ class CallController extends Controller
     public function store(Request $request)
     {
 
-        // $request->validate([
-        //     'file' => 'required',
+        // // $request->validate([
+        // //     'file' => 'required',
             
-        // ]);
+        // // ]);
   
         $file  = $request->file;
         // dd($file);
         $name = time().'.'.$file->getClientOriginalExtension();
         $path = $file->storeAs('public/csv', $name);
         // dd(public_path('storage/csv/'));
-        $users = (new FastExcel)->configureCsv(';', '#', 'gbk')->import(public_path('storage/csv/' .$name), function ($line) {
-            // dd($line);
-                return CallHistory::create([
-                    'caller_id' => $line['id'],
-                    'callere164' => $line['callere164'],
-                    'calleraccesse164' => $line['calleraccesse164'],
-                    'calleee164' => $line['calleee164'],
-                    'calleeaccesse164' => $line['calleeaccesse164'],
-                    'callerip' => $line['callerip'],
-                    'callergatewayh323id' => $line['callergatewayh323id'],
-                    'callerproductid' => $line['callerproductid'],
-                    'callertogatewaye164' => $line['callertogatewaye164'],
-                    'calleeip' => $line['calleeip'],
-                    'calleegatewayh323id' => $line['calleegatewayh323id'],
-                    'calleeproductid' => $line['calleeproductid'],
-                    'calleetogatewaye164' => $line['calleetogatewaye164'],
-                    'billingmode' => $line['billingmode'],
-                    'calllevel' => $line['calllevel'],
-                    'agentfeetime' => $line['agentfeetime'],
-                    'starttime' => $line['starttime'],
-                    'stoptime' => $line['stoptime'],
-                    'pdd' => $line['pdd'],
-                    'holdtime' => $line['holdtime'],
-                    'feeprefix' => $line['feeprefix'],
-                    'feetime' => $line['feetime'],
-                    'fee' => $line['fee'],
-                    'suitefee' => $line['suitefee'],
-                    'suitefeetime' => $line['suitefeetime'],
-                    'incomefee' => $line['incomefee'],
-                    'customername' => $line['customername'],
-                    'agentfeeprefix' => $line['agentfeeprefix'],
-                    'agentfee' => $line['agentfee'],
-                    'agentsuitefee' => $line['agentsuitefee'],
-                    'agentsuitefeetime' => $line['agentsuitefeetime'],
-                    'agentaccount' => $line['agentaccount'],
-                    'agentname' => $line['agentname'],
-                    'flowno' => $line['flowno'],
-                    'softswitchdn' => $line['softswitchdn'],
-                    'enddirection' => $line['enddirection'],
-                    'endreason' => $line['endreason'],
-                    'calleebilling' => $line['calleebilling'],
-                    'cdrlevel' => $line['cdrlevel'],
-                    'subcdr_id' => $line['subcdr_id'],
-                ]);
-        });
+        $users = (new FastExcel)->import(public_path('storage/csv/' .$name), function ($line) {
+            // dd($line['id']);
+                $checkHistory = CallHistory::whereCallerId($line['id'])->first();
+                if(empty($checkHistory)){
+                    return CallHistory::create([
+                        'caller_id' => $line['id'],
+                        'callere164' => $line['callere164'],
+                        'calleraccesse164' => $line['calleraccesse164'],
+                        'calleee164' => $line['calleee164'],
+                        'calleeaccesse164' => $line['calleeaccesse164'],
+                        'callerip' => $line['callerip'],
+                        'callergatewayh323id' => $line['callergatewayh323id'],
+                        'callerproductid' => $line['callerproductid'],
+                        'callertogatewaye164' => $line['callertogatewaye164'],
+                        'calleeip' => $line['calleeip'],
+                        'calleegatewayh323id' => $line['calleegatewayh323id'],
+                        'calleeproductid' => $line['calleeproductid'],
+                        'calleetogatewaye164' => $line['calleetogatewaye164'],
+                        'billingmode' => $line['billingmode'],
+                        'calllevel' => $line['calllevel'],
+                        'agentfeetime' => $line['agentfeetime'],
+                        'starttime' => $line['starttime'],
+                        'stoptime' => $line['stoptime'],
+                        'pdd' => $line['pdd'],
+                        'holdtime' => $line['holdtime'],
+                        'feeprefix' => $line['feeprefix'],
+                        'feetime' => $line['feetime'],
+                        'fee' => $line['fee'],
+                        'suitefee' => $line['suitefee'],
+                        'suitefeetime' => $line['suitefeetime'],
+                        'incomefee' => $line['incomefee'],
+                        'customername' => $line['customername'],
+                        'agentfeeprefix' => $line['agentfeeprefix'],
+                        'agentfee' => $line['agentfee'],
+                        'agentsuitefee' => $line['agentsuitefee'],
+                        'agentsuitefeetime' => $line['agentsuitefeetime'],
+                        'agentaccount' => $line['agentaccount'],
+                        'agentname' => $line['agentname'],
+                        'flowno' => $line['flowno'],
+                        'softswitchdn' => $line['softswitchdn'],
+                        'enddirection' => $line['enddirection'],
+                        'endreason' => $line['endreason'],
+                        'calleebilling' => $line['calleebilling'],
+                        'cdrlevel' => $line['cdrlevel'],
+                        'subcdr_id' => $line['subcdr_id'],
+                    ]);
+                }
+         });
+        return redirect()->back();
     }
-
+    
 }
--- resources/views/call-history.blade.php
+++ resources/views/call-history.blade.php
@@ -42,7 +42,7 @@
                                 <label for="exampleInputFile">File input</label>
                                 <div class="input-group">
                                     <div class="custom-file">
-                                    <input type="file" name="file" class="custom-file-input" id="exampleInputFile">
+                                    <input type="file" name="file" class="custom-file-input" id="exampleInputFile" required>
                                     <label class="custom-file-label" for="exampleInputFile">Choose file</label>
                                     </div>
                                     <div class="input-group-append">
