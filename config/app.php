8603e8524d062a465a4ce7f291ce3ae12b947c70
Nitiz <nitiz143@gmail.com>
2022-08-11 23:42:01 +0530

    update fixes

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
 
     /*
