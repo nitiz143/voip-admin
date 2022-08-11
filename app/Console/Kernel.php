09ad4a3f239a652cb6148498cc156853d44fa638
Nitiz <nitiz143@gmail.com>
2022-08-12 01:07:09 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,42 +28,51 @@ class Kernel extends ConsoleKernel
 
         $tasks = CronJob::all();
 
-        foreach ($tasks as $task) {
-            $frequency = $task->job_time;
-            $minute = 0;
-            $hour = 0;
-            $day = 0;
-            $month = 0;
-            if($frequency == 'everyMinute'){
-                $minute = $task->job_intervel;
-            }elseif ($frequency == 'hourly') {
-                $hour = $task->job_intervel;
-            }elseif ($frequency == 'daily') {
-                $day = $task->job_intervel;
-            }elseif ($frequency == 'monthly') {
-                $month = $task->job_intervel;
-            }
-            $weeks = json_decode($task->job_day);
-            if($task->cron_type == 'Download VOS SFTP File'){
-                // $schedule->command('csvImport:cron')->$frequency();
-                // $schedule->command('download:cron')->$frequency();
-                if(!empty($weeks)){
-                    $week_day = (Carbon::now()->format('D'));
-                    if(in_array($week_day,$weeks)){
-                        $schedule->command('csvImport:cron')->cron( $minute,$hour, $day, $month,$week_day,'*');
-                        $schedule->command('download:cron')->cron( $minute,$hour, $day, $month,$week_day,'*');
-                    }
+        // foreach ($tasks as $task) {
+        //     $frequency = $task->job_time;
+        //     $minute = 0;
+        //     $hour = 0;
+        //     $day = 0;
+        //     $month = 0;
+        //     if($frequency == 'everyMinute'){
+        //         $minute = $task->job_intervel;
+        //     }elseif ($frequency == 'hourly') {
+        //         $hour = $task->job_intervel;
+        //     }elseif ($frequency == 'daily') {
+        //         $day = $task->job_intervel;
+        //     }elseif ($frequency == 'monthly') {
+        //         $month = $task->job_intervel;
+        //     }
+          //  $weeks = json_decode($task->job_day);
+            $schedule->command('download:cron')->cron('0 7 * * * *');
+            $schedule->command('csvImport:cron')->cron('0 9 * * * *');
+        //     if($task->cron_type == 'Download VOS SFTP File'){
+        //         // $schedule->command('csvImport:cron')->$frequency();
+        //         // $schedule->command('download:cron')->$frequency();
+        //         if(!empty($weeks)){
+        //             $week_day = (Carbon::now()->format('D'));
+        //             if(in_array($week_day,$weeks)){
 
-                }
-            }
+        //             }
 
-            //else{
-            //     $schedule->call(function() use($task) {
-            //         /*  Run your task here */
-            //         Log::info($task->job_title.' '.\Carbon\Carbon::now());
-            //     })->monitorName($task->job_title)->$frequency();
-            // }
-        }
+        //         }
+        //     }elseif($task->cron_type == 'Active job Cron Email'){
+        //         if(!empty($weeks)){
+        //             $week_day = (Carbon::now()->format('D'));
+        //             if(in_array($week_day,$weeks)){
+
+        //             }
+
+        //         }
+        //     }
+
+        //     //else{
+        //     //     $schedule->call(function() use($task) {
+        //     //         /*  Run your task here */
+        //     //         Log::info($task->job_title.' '.\Carbon\Carbon::now());
+        //     //     })->monitorName($task->job_title)->$frequency();
+        //     // }
+       // }
     }
 
     /**
