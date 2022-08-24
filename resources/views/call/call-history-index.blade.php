afa4d57cd52e476ba398d03bcf1feb097b67382e
Nitiz <nitiz143@gmail.com>
2022-08-24 22:49:45 +0530

    Update call-history-index.blade.php

--- resources/views/call/call-history-index.blade.php
+++ resources/views/call/call-history-index.blade.php
@@ -57,7 +57,7 @@
                     <div class="modal-dialog modal-lg">
                         <div class="modal-content">
                             <div class="modal-header">
-                              <h5 class="modal-title">Modal title</h5>
+                              <h5 class="modal-title">Call Detail</h5>
                               <button type="button" id="btnModeClose" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                               </button>
