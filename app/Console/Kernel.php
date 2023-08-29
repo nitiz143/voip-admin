74bda0a0fd332e4af7ffc0c28f9012c5fc00409e
Nitiz Sharma <nitiz143@gmail.com>
2023-08-29 12:58:08 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('csvImport:cron')->everyFifteenMinutes();
         $schedule->command('download:cron')->cron('*/18 * * * *')->withoutOverlapping();
         $schedule->command('account:cron')->cron('*/20 * * * *')->withoutOverlapping();
-        $schedule->command('vendor:cron')->cron('*/20* *')->withoutOverlapping();
+        $schedule->command('vendor:cron')->cron('*/20 * * * *')->withoutOverlapping();
         $schedule->command('invoice_create:cron')->daily();
       //  $schedule->command('Billing:cron')->everyFiveMinutes();
     }
