0f2fa3ebc8961460864cef369adc22f635d00e85
Nitiz Sharma <nitiz143@gmail.com>
2023-08-29 17:02:39 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,10 +32,10 @@ class Kernel extends ConsoleKernel
 
 
 
-        $schedule->command('csvImport:cron')->everyNMinutes(15)->withoutOverlapping();
-        $schedule->command('download:cron')->everyNMinutes(18)->withoutOverlapping();
-        $schedule->command('account:cron')->everyNMinutes(20)->withoutOverlapping();
-        $schedule->command('vendor:cron')->everyNMinutes(20)->withoutOverlapping();
+        $schedule->command('csvImport:cron')->cron('*/15 * * * *')->withoutOverlapping();
+        $schedule->command('download:cron')->cron('*/18 * * * *')->withoutOverlapping();
+        $schedule->command('account:cron')->cron('*/20 * * * *')->withoutOverlapping();
+        $schedule->command('vendor:cron')->cron('*/20 * * * *')->withoutOverlapping();
         $schedule->command('invoice_create:cron')->daily();
       //  $schedule->command('Billing:cron')->everyFiveMinutes();
     }
