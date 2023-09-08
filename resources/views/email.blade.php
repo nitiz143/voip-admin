aa923f6fbea37b9b447b657eaa94b9d1cf6aa2cf
Nitiz Sharma <nitiz143@users.noreply.github.com>
2023-09-08 18:43:31 +0530

    update fixes

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,7 +1,17 @@
 <h2>Email</h2>
 <p>Email has been sent.</p>
 <p>Billing_cycle: {{ $data['billing_cycle'] }}</p>
-<p>last_invoice_date: {{ $data['last_invoice_date'] }}</p>
 
-{{-- <p>Next_invoice_date: {{ $data['next_invoice_date'] }}</p> --}}
-{{-- <p>Billing_startdate: {{ $data['billing_startdate'] }}</p> --}}
+    @if($data['billing_cycle'] == "quarterly")
+        <p>next_invoice_date: {{ date('Y-m-d', strtotime($data['next_invoice_date']. ' + 3 month ')); }}</p>
+    @elseif($data['billing_cycle'] == "monthly")
+        <p>next_invoice_date: {{ date('Y-m-d', strtotime($data['next_invoice_date']. ' + 1 month ')); }}</p>
+    @elseif($data['billing_cycle'] == "yearly")
+        <p>next_invoice_date: {{ date('Y-m-d', strtotime($data['next_invoice_date']. ' + 1 year ')); }}</p>
+    @elseif($data['billing_cycle'] == "weekly")
+        <p>next_invoice_date: {{ date('Y-m-d', strtotime($data['next_invoice_date']. ' + 1 week ')); }}</p>
+    @elseif($data['billing_cycle'] == "daily")
+        <p>next_invoice_date: {{ date('Y-m-d', strtotime($data['next_invoice_date']. ' + 1 day ')); }}</p>
+    @else
+        <p>{{ $data->date('Y-m-d', strtotime($data['next_invoice_date']. ' + 1 days')); }}</p>
+    @endif
